a40059d7c6912f7a473bb3869d4a1f5506c57ab4
Tony A. Gomes <tonyadryanogomes1@gmail.com>
2022-08-05 19:08:51 -0300

    setup _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,16 +8,9 @@ import theme from '../styles/chakra/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=PT+Serif:ital,wght@0,700;1,400&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet" />
-      </Head>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
 
